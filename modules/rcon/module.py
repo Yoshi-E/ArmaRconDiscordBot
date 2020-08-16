09748929b628d8246dcf244d9794ad5b97f2000e
Yoshi-E <server19960714@yahoo.de>
2020-08-16 16:27:47 +0200

    test 4

--- modules/rcon/module.py
+++ modules/rcon/module.py
@@ -822,8 +822,8 @@ class CommandRcon(commands.Cog):
         brief="Shows performance information in the dedicated server console. Interval 0 means to stop monitoring.",
         pass_context=True)
     async def monitords(self, ctx, interval = 1): 
-        def sendLoad(event, timestamp, event_match):
-            ctx.send(event_match.group(0))
+        def sendLoad(*args):
+            print(args) #ctx.send(event_match.group(0))
         if(interval > 0):
             self.readLog.EH.add_Event("Server load", sendLoad)
             await self.arma_rcon.monitords(1)
