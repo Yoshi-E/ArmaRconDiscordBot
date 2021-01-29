320552afc3f384286dadbf8fbb60fb676d1a5e2e
unknown <server19960714@yahoo.de>
2021-01-29 17:20:56 +0100

    added python version check

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
+import sys
+import os
+
+if(sys.version_info[0] != 3 or (sys.version_info[1] > 6 or sys.version_info[1] < 4)):
+    sys.exit("This bot requires Python3 >= 3.4 and <= 3.6. You are currently running version {}.{}".format(*sys.version_info[:2]))
 import discord
 from discord.ext import commands
 from modules.core import utils
 import time
 import subprocess
 import asyncio
-import sys
-import os
+
 
 import logging
 from logging.handlers import RotatingFileHandler
