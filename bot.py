74f24ec5e7336b47e6ba334e13d4314a501f7779
unknown <server19960714@yahoo.de>
2021-03-31 01:27:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import sys
 import os
 import time
 
-if(sys.version_info[0] != 3 or (sys.version_info[1] > 5 or sys.version_info[1] < 5)):
+if(sys.version_info[0] != 3 or (sys.version_info[1] > 6 or sys.version_info[1] < 5)):
     print("This bot requires Python >= 3.5 and <= 3.6. You are currently running version {}.{}".format(*sys.version_info[:2]))
     time.sleep(5)
     sys.exit("Terminated")
